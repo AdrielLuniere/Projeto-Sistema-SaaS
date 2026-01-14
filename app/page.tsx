eeada3d16f35faac2f8433c7ec7b5d6dfdec58f2
AdrielLuniere <adrielbinda@gmail.com>
2026-01-14 11:10:04 -0300

    Set up Project Structure (components, lib, hooks, types)

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-slate-900 flex min-h-screen items-center justify-center gap-4">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        Next.js + TypeScript + Tailwind 🚀
+      </h1>
+
+      <Button className="bg-white text-black">
+        Teste Shadcn
+      </Button>
+
+    </main>
+  );
+}
+
+
+
